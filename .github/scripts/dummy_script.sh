ac14881305ce920ae5eea822d32a22993c3f09b3
Karthik <karthik.ms@zohocorp.com>
2020-10-12 21:48:48 +0530

    Trying decrypt_secrets.sh

--- .github/scripts/dummy_script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$PROFILE_DECRYPTION_KEY" == "superSecretPassPhrase1" ]
-then
-  echo "Prov pass Correct"
-else 
-  echo "Prov pass NOT correct"
-fi
--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,39 +1,28 @@
 #!/bin/sh
 set -eo pipefail
 
-# del
-if [ "$PROFILE_DECRYPTION_KEY" == "superSecretPassPhrase1" ]
-then
-  echo "Prov pass Correct"
-else 
-  echo "Prov pass NOT correct"
-fi
-if [ "$CERTIFICATE_DECRYPTION_KEY" == "superSecretPassPhrase1" ]
-then
-  echo "cert pass Correct"
-else
-  echo "cert pass NOT Correct"
-fi
-# del
+PROFILE_NAME="Dummy_Wildcard_iOS_Distribution_Profile.mobileprovision"
 
-
-PROFILE_NAME="ca97b3a8-94d4-466e-837c-5029c83aea58.mobileprovision"
-
+# Decrypting files
 gpg --verbose --batch --yes --decrypt --passphrase="$PROFILE_DECRYPTION_KEY" --output ./.github/secrets/${PROFILE_NAME} ./.github/secrets/${PROFILE_NAME}.gpg
 gpg --verbose --batch --yes --decrypt --passphrase="$CERTIFICATE_DECRYPTION_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
-
 echo "Finished decryption"
 
+
+# Moving Profile to destination
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-
 cp ./.github/secrets/${PROFILE_NAME} ~/Library/MobileDevice/Provisioning\ Profiles/${PROFILE_NAME}
+echo "Moved profile"
 
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+# Adding certificate to keychain
+KEYCHAIN=~/Library/Keychains/build.keychain
+PASS_PHRASE="some"
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+security create-keychain -p $PASS_PHRASE build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k $KEYCHAIN -P $P12_PASSPHRASE -A
+          
+security list-keychains -s $KEYCHAIN
+security default-keychain -s $KEYCHAIN
+security unlock-keychain -p $PASS_PHRASE $KEYCHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k $PASS_PHRASE $KEYCHAIN
